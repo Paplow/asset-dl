ac68f53906314227587d799d19539b2a53c44d0d
Lawrence Onah <paplow01@gmail.com>
2020-02-29 06:35:20 +0100

    :hammer: minor code refactoring

--- asset-dl.py
+++ asset-dl.py
@@ -1,25 +1,9 @@
 #!/usr/bin/python3
 
-"""
-*********************************************************
- _______                      __
-|       \                    |  \
-| $$$$$$$\ ______    ______  | $$  ______   __   __   __
-| $$__/ $$|      \  /      \ | $$ /      \ |  \ |  \ |  \
-| $$    $$ \$$$$$$\|  $$$$$$\| $$|  $$$$$$\| $$ | $$ | $$
-| $$$$$$$ /      $$| $$  | $$| $$| $$  | $$| $$ | $$ | $$
-| $$     |  $$$$$$$| $$__/ $$| $$| $$__/ $$| $$_/ $$_/ $$
-| $$      \$$    $$| $$    $$| $$ \$$    $$ \$$   $$   $$
- \$$       \$$$$$$$| $$$$$$$  \$$  \$$$$$$   \$$$$$\$$$$
-                   | $$
-                   | $$
-                    \$$
-*********************************************************
-"""
-
 from urllib import request as req
 from bs4 import BeautifulSoup
 from shutil import copyfileobj
+from pyfiglet import Figlet
 from re import search
 import argparse
 import sys
@@ -27,13 +11,16 @@ import os
 
 
 def main():
+    f = Figlet(font='slant')
+    print(f.renderText('Asset-dl'))
+
     # Command line parser
     parser = argparse.ArgumentParser(description="Smart Asset Downloader",
                                      argument_default=sys.argv[0] + " -u URL -p PATH",
-                                     epilog="This tool downloads all assets on a page and saves them by directory structure.")
+                                     epilog="Download all assets on a page and saves them by directory structure.")
     required = parser.add_argument_group("Required arguments")
     required.add_argument("-u", "--url", type=str, help="Target site url")
-    required.add_argument("-p", "--path", type=str, help="The absolute path to save assets")
+    required.add_argument("-p", "--path", type=str, help="Path to save assets")
 
     # Print help if no provided arguments
     if len(sys.argv) == 1:
@@ -72,25 +59,31 @@ def req_obj(url):
 # Download assets
 def download_asset(urls, args):
     try:
-        basepath = args.path
+        base_path = args.path
     except IndexError:
-        basepath = os.path.dirname(os.path.abspath(__file__))
+        base_path = os.path.dirname(os.path.abspath(__file__))
 
     line = "#" * 20
-    sys.stdout.write(line + " Downloading " + line + "\r\n")
     for url in urls:
-        if not url or not search("(\.[a-zA-Z0-9]+)$", url):
+        #  Skip link if it's not a file
+        if not url or not search("(\\.[a-zA-Z0-9]+)$", url):
             continue
 
-        path = search("/([a-zA-Z0-9/\-_]+)/", url).group()[1:] if search("/([a-zA-Z0-9/\-_]+)/", url) else ''
-        fullpath = os.path.join(basepath, path)
-        os.system("mkdir -p " + fullpath)
+        path = search("/([a-zA-Z0-9/-_]+)/", url)
+        path = path.group()[1:] if path else ''
+        full_path = os.path.join(base_path, path)
+
+        # Create directory if it doesn't already exists
+        if not os.path.exists(full_path):
+            # os.mkdir(full_path) refuses to create
+            # the directory for some weird reason
+            os.system("mkdir -p " + full_path)
 
         file = url[2:] if url[:2] == './' else url
-        url = url if not search("^(\.\/|\/|(?!(http|www)))", url) else search("(http(s|)://(www.|)([a-zA-Z0-9-.]+))",
-                                                                              args.url).group() + '/' + file
+        link = search("(http(s|)://(www.|)([a-zA-Z0-9-.]+))", args.url).group() + '/' + file
+        url = url if not search("^(./|/|(?!(http|www)))", url) else link
 
-        with req.urlopen(req_obj(url)) as res_data, open(fullpath + url.split('/')[-1], 'wb') as asset:
+        with req.urlopen(req_obj(url)) as res_data, open(full_path + url.split('/')[-1], 'wb') as asset:
             copyfileobj(res_data, asset)
 
         sys.stdout.write(str(url) + "\r\n")
